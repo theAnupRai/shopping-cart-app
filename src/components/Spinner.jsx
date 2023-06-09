7df7b6c2cf02d530ecf74108e3b26808cba46932
theAnupRai <theAnupRai592@gmail.com>
2023-06-10 02:47:03 +0530

    change css of loading spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Spinner = () => {
   return (
-    <div className='spinner'>Loading</div>
-  )
-}
+    <div class="flex justify-center items-center h-screen">
+      <div class="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-gray-900"></div>
+    </div>
+  );
+};
 
-export default Spinner
+export default Spinner;
