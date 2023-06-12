a47a729c69605892649639d8676f48739ba7cb0d
theAnupRai <theAnupRai592@gmail.com>
2023-06-12 07:09:57 +0530

    css added in cart section

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -12,16 +12,16 @@ const CartItem = ({ item, itemIndex }) => {
   };
   return (
     <div>
-      <div>
+      <div className="flex flex-col items-center justify-between hover:scale-110 transition duration ease-in hover:shadow-md gap-3 p-4 mt-10 ml-5 rounded-xl border">
         <div>
-          <img src={item.image} alt="image of item in cart" />
+          <img className="h-[200px]" src={item.image} alt="image of item in cart" />
         </div>
 
         <div>
-          <h2>{item.title}</h2>
-          <h2>{item.description}</h2>
-          <div>
-            <p>{item.price}</p>
+          <h2 className="text-gray-700 font-semibold text-lg text-left truncate mt-1">{item.title}</h2>
+          <h2>{item.description.split(" ").slice(0, 10).join(" ") + "..."}</h2>
+          <div className="flex gap-4 items-center">
+            <p>${item.price}</p>
             <div onClick={removeFromCart}>
               <FcDeleteDatabase />
             </div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -14,32 +14,32 @@ const Cart = () => {
   return (
     <div>
       {cart.length > 0 ? (
-        <div>
-          <div>
+        <div className="flex gap-14 p-4 mt-6 ml-5 rounded-xl">
+          <div className="basis-1/2">
             {cart.map((item, index) => {
               return <CartItem key={item.id} item={item} itemIndex={index} />;
             })}
           </div>
 
-          <div>
+          <div className="mt-20">
             <div>
-              <div>Your cart</div>
-              <div>Summary</div>
+              <div className="text-gray-700 font-semibold text-lg truncate mt-1 basis-1/2">Your cart</div>
+              <div className="text-gray-700 font-semibold text-lg truncate mt-1 basis-1/2">Summary</div>
               <p>
-                <span>Total Items : {cart.length} </span>
+                <span className="text-gray-700 font-semibold text-lg truncate w-40 mt-1 basis-1/2">Total Items : {cart.length} </span>
               </p>
             </div>
 
             <div>
-              <p> Total Amount : ${totalAmount} </p>
+              <p className="text-gray-700 font-semibold text-lg text-left mt-1 basis-1/2"> Total Amount : ${totalAmount} </p>
             </div>
           </div>
         </div>
       ) : (
-        <div>
+        <div className="flex flex-col items-center gap-3 p-4 mt-10 ml-5">
           <h2>Cart Empty</h2>
           <Link to={"/"}>
-            <button>Shop Now</button>
+            <button className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold p-1 px-3 uppercase text-[12px] hover:bg-gray-700 hover:text-white transition duration-300 ease-in">Shop Now</button>
           </Link>
         </div>
       )}
